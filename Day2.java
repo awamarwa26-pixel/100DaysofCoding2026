4c62d28095cbf9e2690733dda83219bf50d065b4
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-03 17:10:52 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,37 @@
+package com.exemple;
+
+public class Main {
+    public static void main(String[] args) {
+        //Data
+        String nama = "Marwa";
+        String prodi = "Informatika";
+        String kelas = "D";
+        int umur = 18;
+        String hobi = "Desain";
+        
+        // 1. println()
+        System.out.println("Biodata Menggunakan Println()");
+        System.out.println("Nama =" + nama);
+        System.out.println("Prodi =" + prodi);
+        System.out.println("Kelas =" + kelas);
+        System.out.println("Umur =" + umur);
+        System.out.println("Hobi =" + hobi);
+        
+        // 2. print()
+        System.out.print("Biodata Menggunakan Print()");
+        System.out.print("Nama =" + nama);
+        System.out.print("Prodi =" + prodi);
+        System.out.print("Kelas =" + kelas);
+        System.out.print("Umur =" + umur);
+        System.out.print("Hobi =" + hobi);
+        
+        // 3. printf()
+        System.out.printf("Biodata Menggunakan Printf()");
+        System.out.printf("Nama =" + nama);
+        System.out.printf("Prodi =" + prodi);
+        System.out.printf("Kelas =" + kelas);
+        System.out.printf("Umur =" + umur);
+        System.out.printf("Hobi =" + hobi);
+        
+    }
+  }
