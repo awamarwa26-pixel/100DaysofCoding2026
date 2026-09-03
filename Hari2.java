875a7a9836c9b6793ec74fcbd436992dca30d87c
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-03 22:07:04 +0800

    Hari2.java

---
+++ Hari2.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // print
+        System.out.print("Nama:" + "Marwa");
+        
+        // println
+        System.out.println();
+        System.out.println("Prodi: " + "Informatika");
+        System.out.println("Alamat: " + "Polewali Mandar");
+        System.out.println("Hobi:  " + "Desain Poster");
+        
+        // printf
+        System.out.printf("Umur: " + "%d tahun%n", 18);
+        }
+}
