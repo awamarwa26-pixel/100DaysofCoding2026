10ce8707a84a5f81c7f3926323eabdf76a53f594
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-04 21:04:44 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,28 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // \n membuat baris barus atau enter 
+
+System.out.println("baris 1 \nbaris 2");
+
+
+// \t ini fungsinya untuk membuat jarak renggang 
+
+System.out.println("nama \t : marwa");
+System.out.println("kelas \t : D");
+System.out.println("Nim \t : D0226042");
+
+// \" menampilkan tanda kutip dua didalam kata
+
+System.out.println("\"Keren\" ");
+
+// \' menampilkan tanda kutip satu dalam kata 
+
+System.out.println("\' Keren \' ");
+
+// \\ menampilkan tanda back slash
+
+System.out.println(" mangga \\ anggur");
+    }
+}
