6e71db381a24e941449778b22c071a88327e4e2b
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-05 21:16:18 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        byte makanan = 20;
+        short minuman = 1500;
+        int harga = 500000;
+        long uang = 1000000000L;
+        
+        System.out.println("Makanan =" + makanan + "\nMinuman =" + minuman + "\nHarga =" + harga + "\nUang =" + uang);
+    }
+          }
