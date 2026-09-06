8defd554768baf1ec88756ccb96234b130f41d9e
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-06 22:30:19 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,11 @@
+package Day5;
+
+public class Main {
+    public static void main(String[] args) {
+        float beratdaging = 3.5f;
+        double hargadaging = 45000.00;
+        
+        System.out.println("Berat Daging \t:" + beratdaging + "kg");
+        System.out.println("Harga Daging \t:" + "Rp" + hargadaging);
+    }
+}
