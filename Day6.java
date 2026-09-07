d518c9961274ee28f1f01da740d807be57fcdcbb
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-07 20:53:25 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Tipe data char dipakai untuk menyimpan 1 huruf/simbol saja.
+        char jeniskelamin = 'P';
+        
+        // Tipe data boolean dipakai untuk kondisi benar atau salah.
+        boolean hujan = false;
+        
+        System.out.println("Jenis Kelamin =" + jeniskelamin);
+        System.out.println("Hujan =" + hujan);
+    }
+}
