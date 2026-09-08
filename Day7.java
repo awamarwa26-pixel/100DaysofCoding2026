982e1abffc242df22503bfafc353df51acbb1c58
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-08 20:46:48 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+          //Tipe Data String untuk menyimpan data berupa teks
+        String nama = "Marwa";
+        String prodi = "Informatika";
+        String univ = "Unsulbar";
+        
+        System.out.println("Nama =" + nama);
+        System.out.println("Prodi =" + prodi);
+        System.out.println("Universitas =" + univ);
+    }
+          }
