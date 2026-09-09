cc1dea6828304c91370a1832af4349adfd04f8a5
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-09 19:07:19 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Deklarasi variabel, membuat nama variabel
+
+String hari;
+int uang_jajan;
+
+
+// inisialisasi variabel, mengisi semua data variabel dengan tipe data tertentu
+
+hari = "Senin";
+uang_jajan = 10000;
+System.out.println("Hari \t\t= "+ hari);
+System.out.println("Uang jajan \t= "+uang_jajan);
+
+
+// melakukan update data pada fariabel umur dan uang jajan
+
+hari = "Selasa";
+uang_jajan = 5000;
+
+System.out.println();
+
+System.out.println("Hari \t\t= "+ hari);
+System.out.println("Uang jajan \t= "+uang_jajan);
+    }
+}
