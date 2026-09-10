add36c3162537c7782ee3e1396bd934e1bceae6f
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-10 20:21:59 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+           //Konstanta : nilai tetap yang tidak bisa di ubah.
+        final int umur = 18;
+        final int tahun = 2008;
+        
+        System.out.println("Umur \t:" + umur);
+        System.out.println("Tahun \t:" + tahun);
+    }
+}
