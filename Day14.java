01232ae7cbd2a06b7d53345feb62b2d602eb2276
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-15 20:36:23 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+        int a = 77;
+        int b = 22;
+        
+        int tambah = a + b;
+        int kurang = a - b;
+        
+        System.out.println("Hasil Penjumlahan \t:" + tambah);
+        System.out.println("Hasil Pengurangan \t:" + kurang);
+    }
+}
