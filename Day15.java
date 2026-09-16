626d7b73337e41607a6cc73bc830c28c7c3ce6d2
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-16 22:38:58 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Perkalian
+        int a = 22;
+        int b = 77;
+        int hasilk = a*b;
+        System.out.println("Hasil Perkalian \t:" + hasilk);
+        
+        //Pembagian
+        int c = 50;
+        int d = 10;
+        int hasilp = c/d;
+        System.out.println("Hasil Pembagian \t:" + hasilp);
+    }
+}
