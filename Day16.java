61a5f6876385948e8f0d198a4af8abde6061b7b1
awamarwa26-pixel <awamarwa26@gmail.com>
2026-09-17 21:41:32 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Modulus atau sisa bagi
+        int a = 77;
+        int b = 22;
+        int hasil = a % b;
+        
+        System.out.println("Angka Pertama \t:" + a);
+        System.out.println("Angka Kedua \t:" + b);
+        System.out.println("Hasil Modulus \t:" + hasil);
+    }
+          }
